efa1edf71078e6c4e43bcbca22270a918d4e2adf
SatheesA <zadizz@gmail.com>
2018-12-11 22:34:07 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import colors from "vuetify/es5/util/colors";
+
+Vue.config.productionTip = false;
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blueGrey.darken2,
+    secondary: colors.grey.lighten3,
+    accent: colors.cyan.darken4,
+    error: colors.red.base,
+    warning: colors.yellow.base,
+    info: colors.blue.base,
+    success: colors.green.base
+  }
+});
+
+/* eslint-disable no-new */
+new Vue({
+  el: "#app",
+  components: { App },
+  template: "<App/>"
+});
